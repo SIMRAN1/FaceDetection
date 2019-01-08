9db8c0baf2c5d54ba1fb1fd6e109adae851f8c44
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2019-01-08 17:18:57 +0530

    Setting Up image View Programitically

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        activityIndicator.startAnimating()
+        setupImageView()
+    }
+    func setupImageView() {
+        guard let image = UIImage(named: "face") else {
+            return
+        }
+        let imagView = UIImageView(image: image)
+        imagView.contentMode = .scaleAspectFit
+        let scaledHeight = (view.frame.width/image.size.width)*image.size.height
+        imagView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
+        imagView.layer.borderColor = UIColor.white.cgColor
+        imagView.layer.borderWidth = 2
+        view.addSubview(imagView)
     }
 
 
